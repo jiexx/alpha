c6e6ae190885fbb311e7f196c28de05bf21e6b2c
jluo <jluo@q.com>
2015-02-17 12:00:14 +0800

    housekeeping

--- vpn/src/com/jiexx/comm/Message.java
+++
@@ -1,8 +0,0 @@
-package com.jiexx.comm;
-
-public abstract interface Message {
-	public void onRequest( Dispatcher d ) ;
-	public void onResponse( Dispatcher d ) ;
-	public void onData( Data d ) ;
-	public void onFailed( String info ) ;
-}
--- vpn/src/com/jiexx/comm/Profile.java
+++
@@ -1,5 +0,0 @@
-package com.jiexx.comm;
-
-public class Profile {
-	public final static String HOST = "";
-}
