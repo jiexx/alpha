e818043db1397ae3f7fb712124d43dedb5ef82a6
roger <imiracle@live.cn>
2015-03-02 23:17:53 +0800

    findContours

--- paipai/recognize/recognize.cpp
+++ paipai/recognize/recognize.cpp
@@ -3,6 +3,7 @@
 #include "stdafx.h"
 
 #include "recognize.h"
+#define BORDER 1
 
 recognize::recognize(){
 	mNormSize = 32;
@@ -51,7 +52,7 @@ void recognize::prepare(){
 			//IplImage* ipl = cvCreateImage( cvSize( mNormSize, mNormSize ), IPL_DEPTH_32F, 1 );
 			//cvConvertScale(img, ipl, 1, 0);
 			cvGetSubRect( img, &dat, cvRect(0,0,mNormSize,mNormSize) );
-			
+
 
 			CvMat row_header, *row1;
 			row1 = cvReshape( &dat, &row_header, 0, 1 );
@@ -107,21 +108,21 @@ IplImage* capture(HWND hWnd) {
 
 	RECT rect;  
 	GetWindowRect(hWnd, &rect);  
-  
+
 	int nWidht = rect.right - rect.left;  
 	int nHeight = rect.bottom - rect.top;  
-  
+
 	HDC hDC = GetWindowDC(hWnd);  
 	HDC hComatibleDC = CreateCompatibleDC(hDC);    
 	HBITMAP hBMP = CreateCompatibleBitmap(hDC, nWidht, nHeight);  
 	HBITMAP hOldBMP = (HBITMAP)SelectObject(hComatibleDC, hBMP);  
 	BitBlt(hComatibleDC, 0, 0, nWidht, nHeight, hDC, 0, 0, SRCCOPY);  
-  
+
 	BITMAP bitmap = {0};  
 	GetObject(hBMP, sizeof(BITMAP), &bitmap);  
 	BITMAPINFOHEADER bi = {0};  
 	BITMAPFILEHEADER bf = {0};  
-      
+
 	CONST int nBitCount = 24;  
 	bi.biSize = sizeof(BITMAPINFOHEADER);  
 	bi.biWidth = bitmap.bmWidth;  
@@ -130,30 +131,30 @@ IplImage* capture(HWND hWnd) {
 	bi.biBitCount = nBitCount;  
 	bi.biCompression = BI_RGB;  
 	DWORD dwSize = ((bitmap.bmWidth * nBitCount + 31) / 32) * 4 * bitmap.bmHeight;  
-  
+
 	HANDLE hDib = GlobalAlloc(GHND, dwSize + sizeof(BITMAPINFOHEADER));  
 	LPBITMAPINFOHEADER lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);  
 	*lpbi = bi;  
-  
+
 	GetDIBits(hComatibleDC, hBMP, 0, bitmap.bmHeight, (BYTE*)lpbi + sizeof(BITMAPINFOHEADER), (BITMAPINFO*)lpbi, DIB_RGB_COLORS);  
-   
+
 	//bf.bfType = 0x4d42;  
 	//dwSize += sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);  
 	//bf.bfSize = dwSize;  
 	//bf.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);  
-  
+
 	//file.Write((BYTE*)&bf, sizeof(BITMAPFILEHEADER));  
 	//file.Write((BYTE*)lpbi, dwSize);  
 	IplImage* result = bmp2ipl((BITMAPINFO*)lpbi, (BYTE*)lpbi + sizeof(BITMAPINFOHEADER));
-      
+
 	GlobalUnlock(hDib);  
 	GlobalFree(hDib);  
-      
+
 	SelectObject(hComatibleDC, hOldBMP);  
 	DeleteObject(hBMP);  
 	DeleteDC(hComatibleDC);  
 	ReleaseDC(hWnd, hDC);  
-  
+
 	return result;  
 } 
 
@@ -171,16 +172,20 @@ vector<Piece> recognize::identify(IplImage* img) {
 	//cvThreshold(img, img, 0, 255, CV_THRESH_BINARY);  
 	//imwrite( "Smooth.jpg", Mat(img,0) ); 
 
-	Mat input(img, 0), gray, binary, erosion, element;
+	Mat input(img, 0), gray, binary, erosion, dilation, element, markers;
 	cvtColor(input, gray, CV_BGR2GRAY); 
-
+	imwrite( "input.jpg", input ); 
 	threshold(gray, binary, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);/*binary image*/
 
-	element = getStructuringElement(MORPH_RECT/*erosion_type*/, Size(2,2), Point( -1, -1 )/*Size(-2*1erosion_size+1, -2*1erosion_size+1)*/); 
-	/*dilate*/erode(binary, erosion, element);  
-
-	imwrite( "erode.jpg", erosion ); 
-
+	element = getStructuringElement(MORPH_RECT/*erosion_type*/, Size(2,2), Point(-1,-1) ); 
+	erode(binary, erosion, element);
+	imwrite( "erode.jpg", erosion );  
+	element = getStructuringElement(MORPH_RECT, Size(2,2), Point(-1,-1));
+	dilate(erosion, dilation, element, Point(-1,-1), 6);
+	//threshold(dilation,dilation,0,255,THRESH_BINARY_INV);
+	imwrite( "dilation.jpg", dilation );
+	//markers = erosion + dilation;
+	//imwrite( "dilation2.jpg", markers );
 	/////////split////////
 	vector< vector< Point> > contours;    
 	findContours(erosion, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);   
@@ -191,7 +196,7 @@ vector<Piece> recognize::identify(IplImage* img) {
 		//Scalar color = Scalar(0, 0, 0);/*bb outline*/    
 		boundRect[i] = boundingRect(Mat(contours[i]));    
 		//rectangle(erosion, boundRect[i].tl(), boundRect[i].br(), color, 0.2, 8, 0);    
-        
+
 		CvRect roi = CvRect(boundRect[i]);    
 		IplImage orig = erosion;    
 		IplImage *src = cvCreateImage(cvSize(roi.width, roi.height), orig.depth, orig.nChannels);    
@@ -200,14 +205,14 @@ vector<Piece> recognize::identify(IplImage* img) {
 		cvResetImageROI(&orig);    
 
 		imwrite( "roi.jpg", Mat(src,0) ); 
-    
+
 		IplImage *one;    
 		one = cvCreateImage(cvSize(mNormSize, mNormSize), IPL_DEPTH_8U, 1);    
 		cvResize(src, one);    
 		cvThreshold(one, one, 0, 255, CV_THRESH_BINARY);  
 
 		imwrite( "Threshold.jpg", Mat(one,0) ); 
-    
+
 		int ret = classify(one);       
 		result.push_back( Piece(boundRect[i].tl().x,ret) );
 	}
@@ -218,9 +223,16 @@ vector<Piece> recognize::identify(IplImage* img) {
 }
 
 vector<Piece> recognize::test(const char* file) {
-	Mat img = imread(file);
-	Mat dst;
-	img.convertTo(dst, CV_8UC1);
+	//Mat img = imread(file);
+	//Mat dst;
+	//img.convertTo(dst, CV_8UC1);
+	//IplImage ipl = IplImage(dst); 
+	IplImage* img = cvLoadImage(file, -1);
+	CvMat* mat = cvGetSubRect(img, 
+		cvCreateMatHeader(img->width-2*BORDER, img->height-2*BORDER, CV_8UC1), 
+			cvRect(BORDER,BORDER,(img->width-2*BORDER),(img->height-2*BORDER)) );
+	//IplImage* ipl = cvCreateImage(cvGetSize(mat),img->depth,img->nChannels);
+	Mat dst(mat);
 	IplImage ipl = IplImage(dst); 
 	return identify(&ipl);
 }
